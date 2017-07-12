88971b9a0a7019000ee33ba234e9bb4a909980e9
dweb1 <david.p.weber@gmail.com>
2017-07-12 15:58:20 -0400

    Added First Question object to game.js and bootstrap reference links in index.html

--- game.js
+++ game.js
@@ -1,4 +1,36 @@
 $('.questionButton').on('click', function() {
-	console.log(event.target.id)
-	// event.target.Questions.poseQuestion;
+	var buttonClicked = event.target.id;
+	if (buttonClicked === "firstQuestionButton") {
+		console.log(firstQuestion.question);
+	}
 	});
+
+var firstQuestion = {
+	question: "Famous for his characters Horton, the Cat in the Hat and Suzy Lou Who, this author remains a staple in most nursery school libraries.",
+	rightAnswer: "Who is Dr. Seuss?",
+	wrongAnswers: ["Who is A.A. Milne?", "Who is Roald Dahl?", "Who is Hans Christian Andersen?"]
+}
+
+var secondQuestion = {
+	question: "",
+	rightAnswer: "",
+	wrongAnswers: []
+}
+
+var thirdQuestion = {
+	question: "",
+	rightAnswer: "",
+	wrongAnswers: []
+}
+
+var fourthQuestion = {
+	question: "",
+	rightAnswer: "",
+	wrongAnswers: []
+}
+
+var fifthQuestion = {
+	question: "",
+	rightAnswer: "",
+	wrongAnswers: []
+}
