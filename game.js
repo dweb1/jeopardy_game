be11ba2e63aa4b6c719a67b60c441566fa253ba8
dweb1 <david.p.weber@gmail.com>
2017-07-12 16:46:49 -0400

    Added game logic to game.js with if/then statements. Built out objects for each question, as well

--- game.js
+++ game.js
@@ -1,7 +1,35 @@
 $('.questionButton').on('click', function() {
 	var buttonClicked = event.target.id;
 	if (buttonClicked === "firstQuestionButton") {
-		console.log(firstQuestion.question);
+		$('.modal-header').html(firstQuestion.question);
+		$('#firstOption').html(firstQuestion.rightAnswer);
+		$('#secondOption').html(firstQuestion.wrongAnswers[0]);
+		$('#thirdOption').html(firstQuestion.wrongAnswers[1]);
+		$('#fourthOption').html(firstQuestion.wrongAnswers[2]);
+	} else if (buttonClicked === "secondQuestionButton") {
+		$('.modal-header').html(secondQuestion.question);
+		$('#firstOption').html(secondQuestion.rightAnswer);
+		$('#secondOption').html(secondQuestion.wrongAnswers[0]);
+		$('#thirdOption').html(secondtQuestion.wrongAnswers[1]);
+		$('#fourthOption').html(secondQuestion.wrongAnswers[2]);
+	} else if (buttonClicked === "thirdQuestionButton") {
+		$('.modal-header').html(thirdQuestion.question);
+		$('#firstOption').html(thirdQuestion.rightAnswer);
+		$('#secondOption').html(thirdQuestion.wrongAnswers[0]);
+		$('#thirdOption').html(thirdQuestion.wrongAnswers[1]);
+		$('#fourthOption').html(thirdQuestion.wrongAnswers[2]);
+	} else if (buttonClicked === "fourthQuestionButton") {
+		$('.modal-header').html(fourthQuestion.question);
+		$('#firstOption').html(fourthQuestion.rightAnswer);
+		$('#secondOption').html(fourthQuestion.wrongAnswers[0]);
+		$('#thirdOption').html(fourthQuestion.wrongAnswers[1]);
+		$('#fourthOption').html(fourthQuestion.wrongAnswers[2]);
+	} else if (buttonClicked === "fifthQuestionButton") {
+		$('.modal-header').html(fifthQuestion.question);
+		$('#firstOption').html(fifthQuestion.rightAnswer);
+		$('#secondOption').html(fifthQuestion.wrongAnswers[0]);
+		$('#thirdOption').html(fifthQuestion.wrongAnswers[1]);
+		$('#fourthOption').html(fifthQuestion.wrongAnswers[2]);
 	}
 	});
 
@@ -12,9 +40,9 @@ var firstQuestion = {
 }
 
 var secondQuestion = {
-	question: "",
-	rightAnswer: "",
-	wrongAnswers: []
+	question: "Drawing on inspiration from his time in Spain during that country's civil war, Ernest Hemingway wrote this novel while on vacation in Cuba in the late 1930s.",
+	rightAnswer: "What is 'For Whom the Bell Tolls?",
+	wrongAnswers: ["What is 'The Sun Also Rises'?", "What is 'The Old Man and the Sea'?", "What is 'A Farewell to Arms'?"]
 }
 
 var thirdQuestion = {
@@ -24,9 +52,9 @@ var thirdQuestion = {
 }
 
 var fourthQuestion = {
-	question: "",
-	rightAnswer: "",
-	wrongAnswers: []
+	question: "Before her untimely death from lupus at the age of 39, this author's work focused on the literary trope of the grotesque in the American South.",
+	rightAnswer: "Who is Flannery O'Connor?",
+	wrongAnswers: ["Who is Sylvia Plath?", "Who is Zora Neale Hurston?", "Who is Louisa May Alcott?"]
 }
 
 var fifthQuestion = {
